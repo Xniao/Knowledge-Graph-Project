fe0f3b4a332888cea29e2df0c781bf521d9cba64
Xniao <1733487115@qq.com>
2023-12-24 11:31:31 +0800

    Update

--- demo/load_textbooks.py
+++ demo/load_textbooks.py
@@ -1,12 +1,28 @@
 import json
 import os
 
+COUNT = 0
+
+def dfs(node):
+    global COUNT
+    if node == None:
+        return
+    if type(node) == str:
+        COUNT += 1
+        print(node)
+        return
+    COUNT += 1
+    print(node['name'])
+    for item in node['items']:
+        dfs(item)
+
 def main():
     with open('..\\data\\textbooks.json', 'r', encoding='utf-8') as file:
         textbooks = json.load(file)
     for textbook in textbooks:
-        print(textbook)
-    pass
+        dfs(textbook)
+    global COUNT
+    print(COUNT)
 
 if __name__ == '__main__':
     main()
